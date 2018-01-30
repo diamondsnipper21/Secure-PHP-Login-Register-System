d2bdfdca343508c703d0bd0d6ea53a6215afc672
diamondsnipper21 <diamondsnipper21@gmail.com>
2018-01-30 14:10:49 -0500

    Create SessionInterface.php

---
+++ engine/SessionInterface.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This file is a part of secure-php-login-system.
+ *
+ * @author Akbar Hashmi (Owner)           <me@akbarhashmi.com>.
+ * @author Nicholas English (Contributor) <nenglish0820@outlook.com>.
+ *
+ * @link    <https://github.com/akbarhashmi/Secure-PHP-Login-System> Github repository.
+ * @license <https://github.com/akbarhashmi/Secure-PHP-Login-System/blob/master/LICENSE> MIT license.
+ */
+ 
+namespace Akbarhashmi\Engine;
+
+/**
+ * SessionInterface.
+ */
+interface SessionInterface
+{
+}
