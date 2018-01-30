a304450b6a674cdad6c74d9f4faf607c5e7f4572
diamondsnipper21 <diamondsnipper21@gmail.com>
2018-01-30 13:47:58 -0500

    Update server.php

--- server.php
+++ server.php
@@ -1,1 +1,21 @@
+<?php
+/**
+ * This file is a part of secure-php-login-system.
+ *
+ * @author Akbar Hashmi (Owner)           <me@akbarhashmi.com>.
+ * @author Nicholas English (Contributor) <nenglish0820@outlook.com>
+ *
+ * @link    <https://github.com/akbarhashmi/Secure-PHP-Login-System> Github repository.
+ * @license <https://github.com/akbarhashmi/Secure-PHP-Login-Sytem/blob/master/LICENSE> MIT license.
+ */
+
+define('SYSTEM_ROOT', __DIR__);
+
+if (file_exists(SYSTEM_ROOT . '/vendor/autoload.php'))
+{
+    throw new RuntimeException('You need to run composer install or else the system will not run.');
+}
+
+require SYSTEM_ROOT . '/vendor/autoload.php';
+
 
