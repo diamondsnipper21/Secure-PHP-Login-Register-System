bfd208a662e0cb937a1e7512cbd6beda61309a88
diamondsnipper21 <diamondsnipper21@gmail.com>
2018-01-30 14:07:59 -0500

    Update Session.php

--- engine/Session.php
+++ engine/Session.php
@@ -3,7 +3,7 @@
  * This file is a part of secure-php-login-system.
  *
  * @author Akbar Hashmi (Owner)           <me@akbarhashmi.com>.
- * @author Nicholas English (Contributor) <nenglish0820@outlook.com>
+ * @author Nicholas English (Contributor) <nenglish0820@outlook.com>.
  *
  * @link    <https://github.com/akbarhashmi/Secure-PHP-Login-System> Github repository.
  * @license <https://github.com/akbarhashmi/Secure-PHP-Login-System/blob/master/LICENSE> MIT license.
